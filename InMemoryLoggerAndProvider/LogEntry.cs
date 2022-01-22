614982f487bd975e0bac038be758a71f38604376
Shiv Kumar <shiv.kmr003@gmail.com>
2022-01-22 13:44:28 -0500

    Added Exception as a parameter to the LogEntry Type

--- InMemoryLoggerAndProvider/LogEntry.cs
+++ InMemoryLoggerAndProvider/LogEntry.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 
 namespace InMemoryLoggerAndProvider
 {
-    internal sealed record LogEntry(LogLevel LogLevel, EventId EventId, IReadOnlyList<KeyValuePair<string, object>> KeyValuePairs, string Message);
+    internal sealed record LogEntry(LogLevel LogLevel, EventId EventId, IReadOnlyList<KeyValuePair<string, object>> KeyValuePairs, string Message, Exception? exception);
 }
